371e3c40fc1c9a7c6c501fca6c436ca42b8c73ad
Hosam Abuelhiga <102473217+hosam10@users.noreply.github.com>
2025-02-02 18:35:40 +0200

    2.2

--- code_coverage_4/src/main/java/code_coverage_4/code_coverage_4/Power_calculator.java
+++ code_coverage_4/src/main/java/code_coverage_4/code_coverage_4/Power_calculator.java
@@ -1,17 +1,9 @@
 package code_coverage_4.code_coverage_4;
 
 public class Power_calculator {
-	private Multiplier multiplier;
-
-    public Power_calculator(Multiplier multiplier) {
-        this.multiplier = multiplier;
-    }
 
     public double power(double base, double exponent) {
-        double result = base;
-        for (int i = 1; i < exponent; i++) {
-            result = multiplier.multiply(result, base);
-        }
-        return result;
+        return Math.pow(base, exponent);
     }
 }
+
